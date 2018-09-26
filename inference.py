f1dd1abfdf712840037a483c42310c71c0b424ad
banr1jnts <ban.ku.psjbhts.wbtv.0531@gmail.com>
2018-09-27 03:20:44 +0900

    shorten variable names

--- inference.py
+++ inference.py
@@ -17,14 +17,14 @@ from params import args
 import models, naives, losses
 
 if __name__ == '__main__':
-    model_name = args.model
-    model_version = args.version
-    optimizer_name = args.optimizer
+    mdl_name = args.model
+    ver = args.version
+    opt_name = args.optimizer
     learning_rate = args.learning_rate
     decay = args.decay
-    loss_name = args.loss
+    ls_name = args.loss
     stateful = args.stateful
-    lookback = args.lookback
+    lb = args.lookback
     batch_size = args.batch_size
     epochs = args.epochs
     num_filters = args.num_filters
@@ -33,16 +33,16 @@ if __name__ == '__main__':
     random_seed = args.random_seed
     tensorboard = args.tensorboard
     naive_period = args.naive_period
-    start_day = args.start_day
-    split_day_1 = args.split_day_1
-    split_day_2 = args.split_day_2
-    end_day = args.end_day
+    st_day = args.start_day
+    sp1_day = args.split_day_1
+    sp2_day = args.split_day_2
+    ed_day = args.end_day
     input_raw_dir = args.input_raw_dir
     input_preprocessed_dir = args.input_preprocessed_dir
     log_dir = args.log_dir
-    verbose = args.verbose
+    vb = args.verbose
     record = args.record
-    date_format = '%Y-%m-%d'
+    fmt = '%Y-%m-%d'
     os.environ['PYTHONHASHSEED'] = '0'
     np.random.seed(random_seed)
     random.seed(random_seed)
@@ -56,27 +56,27 @@ def get_default_lr(optimizer_name):
     return dict[optimizer_name]
 
 def get_period(start_day, split_day_1, split_day_2, end_day):
-    train_start = start_day
-    train_end = split_day_1 - datetime.timedelta(days=1)
-    train_period = (train_end - train_start).days + 1
-    val_start = split_day_1
-    val_end = split_day_2 - datetime.timedelta(days=1)
-    val_period = (val_end - val_start).days + 1
-    test_start = split_day_2
-    test_end = end_day
-    test_period = (test_end - test_start).days + 1
-    print('train    period: {} ~ {} ({:0=4}days)'.format(train_start.strftime(date_format),
-                                                         train_end.strftime(date_format),
+    train_st = start_day
+    train_ed = split_day_1 - datetime.timedelta(days=1)
+    train_period = (train_ed - train_st).days + 1
+    val_st = split_day_1
+    val_ed = split_day_2 - datetime.timedelta(days=1)
+    val_period = (val_ed - val_st).days + 1
+    test_st = split_day_2
+    test_ed = end_day
+    test_period = (test_ed - test_st).days + 1
+    print('train    period: {} ~ {} ({:0=4}days)'.format(train_st.strftime(fmt),
+                                                         train_ed.strftime(fmt),
                                                          train_period))
-    print('validate period: {} ~ {} ({:0=4}days)'.format(val_start.strftime(date_format),
-                                                         val_end.strftime(date_format),
+    print('validate period: {} ~ {} ({:0=4}days)'.format(val_st.strftime(fmt),
+                                                         val_ed.strftime(fmt),
                                                          val_period))
-    print('test     period: {} ~ {} ({:0=4}days)'.format(test_start.strftime(date_format),
-                                                         test_end.strftime(date_format),
+    print('test     period: {} ~ {} ({:0=4}days)'.format(test_st.strftime(fmt),
+                                                         test_ed.strftime(fmt),
                                                          test_period))
     return train_period, val_period, test_period
 
-def find_class_by_name(name, modules):
+def find_class(name, modules):
     modules = [getattr(module, name, None) for module in modules]
     return next(a for a in modules if a)
 
@@ -127,7 +127,7 @@ def get_daily_data(df, start, end, dummy_col):
     df = pd.concat([col, df], join='outer', sort=False)
     df = df.fillna(0)
     df = df.groupby('time').sum()
-    df = df[start: end] #終了日も含む
+    df = df[start: end]
     idx = pd.DataFrame(index=pd.date_range(start, end))
     df = pd.merge(idx, df, how='outer', left_index=True, right_index=True)
     df = df.fillna(0)
@@ -207,7 +207,7 @@ def main():
     sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
     K.set_session(sess)
 
-    train_period, val_period, test_period = get_period(start_day, split_day_1, split_day_2, end_day)
+    train_period, val_period, test_period = get_period(st_day, sp1_day, sp2_day, ed_day)
     raw_files = sorted(glob.glob(input_raw_dir + 'h????'))
     csv_file = input_preprocessed_dir + 'df.csv'
     if not os.path.exists(csv_file):
@@ -217,55 +217,55 @@ def main():
     df_m2 = df[df['magnitude'] >= 20]
     df_m2 = get_grid_data(df_m2)
     latlon = np.sort(df_m2['latlon'].unique())
-    df_m2 = get_daily_data(df_m2, start=start_day, end=end_day, dummy_col=latlon)
+    df_m2 = get_daily_data(df_m2, start=st_day, end=ed_day, dummy_col=latlon)
     df_m4 = df[df['magnitude'] >= 40]
     df_m4 = get_grid_data(df_m4)
-    df_m4 = get_daily_data(df_m4, start=start_day, end=end_day, dummy_col=latlon)
-    float_data_m2 = df_m2.values.astype(np.float64)
-    float_data_m4 = df_m4.values.astype(np.float64)
-    max_m2 = float_data_m2.max(axis=(0,1))
-    max_m4 = float_data_m4.max(axis=(0,1))
-    float_data_m2 = float_data_m2 * max_m4 / max_m2
-    plt.hist(float_data_m2[:train_period].sum(axis=0), bins=10)
+    df_m4 = get_daily_data(df_m4, start=st_day, end=ed_day, dummy_col=latlon)
+    data_m2 = df_m2.values.astype(np.float64)
+    data_m4 = df_m4.values.astype(np.float64)
+    max_m2 = data_m2.max(axis=(0,1))
+    max_m4 = data_m4.max(axis=(0,1))
+    data_m2 = data_m2 * max_m4 / max_m2
+    plt.hist(data_m2[:train_period].sum(axis=0), bins=10)
     plt.savefig(log_dir + 'fig_train_hist.png', transparent=True, bbox_inches='tight')
-    target_length = float_data_m4.shape[1]
-    float_data = np.hstack([float_data_m2, float_data_m4])
-    print('data shape: {}'.format(float_data.shape))
+    target_length = data_m4.shape[1]
+    data = np.hstack([data_m2, data_m4])
+    print('data shape: {}'.format(data.shape))
 
-    lr = learning_rate if learning_rate else get_default_lr(optimizer_name)
-    optimizer = find_class_by_name(optimizer_name, [keras.optimizers])(lr=lr, decay=decay)
-    loss = find_class_by_name(loss_name, [losses, keras.losses])
-    pre_mean_loss = find_class_by_name(loss_name.replace('mean_', ''), [losses])
+    lr = learning_rate if learning_rate else get_default_lr(opt_name)
+    optimizer = find_class(opt_name, [keras.optimizers])(lr=lr, decay=decay)
+    loss = find_class(ls_name, [losses, keras.losses])
+    pre_mean_loss = find_class(ls_name.replace('mean_', ''), [losses])
 
-    train_gen = generator(float_data,
-                          lookback=lookback,
+    train_gen = generator(data,
+                          lookback=lb,
                           min_idx=0,
                           max_idx=train_period - 1,
                           batch_size=batch_size,
                           target_length=target_length)
-    val_gen = generator(float_data,
-                        lookback=lookback,
+    val_gen = generator(data,
+                        lookback=lb,
                         min_idx=train_period,
                         max_idx=train_period + val_period - 1,
                         batch_size=batch_size,
                         target_length=target_length)
-    test_gen = generator(float_data,
-                         lookback=lookback,
+    test_gen = generator(data,
+                         lookback=lb,
                          min_idx=train_period + val_period,
                          max_idx=None,
                          batch_size=batch_size,
                          target_length=target_length)
 
-    train_steps = (train_period - lookback) // batch_size
-    val_steps = (val_period - lookback) // batch_size
-    test_steps = (len(float_data) - (train_period + val_period) - lookback) // batch_size
-    naive_class = find_class_by_name('Poissonnaive', [naives])()
-    model_class = find_class_by_name(model_name, [models])()
-    naive = naive_class.build_naive(float_data,
+    train_steps = (train_period - lb) // batch_size
+    val_steps = (val_period - lb) // batch_size
+    test_steps = (len(data) - (train_period + val_period) - lb) // batch_size
+    naive_class = find_class('Poissonnaive', [naives])()
+    model_class = find_class(mdl_name, [models])()
+    naive = naive_class.build_naive(data,
                                     batch_size=batch_size,
                                     target_length=target_length)
-    model = model_class.build_model(float_data,
-                                    lookback=lookback,
+    model = model_class.build_model(data,
+                                    lookback=lb,
                                     batch_size=batch_size,
                                     optimizer=optimizer,
                                     loss=loss,
@@ -275,12 +275,12 @@ def main():
                                     dropouts=dropouts,
                                     recurrent_dropouts=recurrent_dropouts)
     model.summary()
-    print('optimizer: {} (lr={})\nloss: {}\n'.format(optimizer_name, lr, loss_name))
+    print('optimizer: {} (lr={})\nloss: {}\n'.format(opt_name, lr, ls_name))
     callbacks = [
-        EarlyStopping(monitor='val_loss', patience=5, verbose=verbose),
-        ModelCheckpoint(filepath=log_dir + 'ckpt_{}{}.h5'.format(model_name, model_version),
-                        monitor='val_loss', save_best_only=True, verbose=verbose),
-        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=verbose),
+        EarlyStopping(monitor='val_loss', patience=5, verbose=vb),
+        ModelCheckpoint(filepath=log_dir + 'ckpt_{}{}.h5'.format(mdl_name, ver),
+                        monitor='val_loss', save_best_only=True, verbose=vb),
+        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=vb),
         ]
     if tensorboard:
         callbacks.append(TensorBoard(log_dir=log_dir + 'tensorboard/', batch_size=batch_size))
@@ -291,7 +291,7 @@ def main():
                                   validation_data=val_gen,
                                   validation_steps=val_steps,
                                   callbacks=callbacks,
-                                  verbose=verbose)
+                                  verbose=vb)
 
     loss = history.history['loss']
     val_loss = history.history['val_loss']
@@ -302,7 +302,7 @@ def main():
     plt.ylim(ymin=0)
     plt.title('Training and validation loss')
     plt.legend()
-    plt.savefig(log_dir + 'fig_{}{}_loss.png'.format(model_name, model_version), transparent=True, bbox_inches='tight')
+    plt.savefig(log_dir + 'fig_{}{}_loss.png'.format(mdl_name, ver), transparent=True, bbox_inches='tight')
 
     print('【evaluation】')
     bin_true, day_true, true = get_test_true(test_gen, test_steps)
@@ -311,31 +311,31 @@ def main():
     md_bin_pred, md_day_pred, md_pred, md_bin_eval, md_day_eval, md_eval = model_evaluate(
             test_gen, test_steps, pre_mean_loss, target_length, model)
     print('Naivemodel: {}'.format(nv_eval))
-    print('{}{}: {}'.format(model_name, model_version, md_eval))
+    print('{}{}: {}'.format(mdl_name, ver, md_eval))
 
-    df_day_true = pd.DataFrame(day_true, index=pd.date_range(end=end_day, periods=93), columns=['True value'])
-    df_nv_day_pred = pd.DataFrame(nv_day_pred, index=pd.date_range(end=end_day, periods=93), columns=['Naive predicton'])
-    df_md_day_pred = pd.DataFrame(md_day_pred, index=pd.date_range(end=end_day, periods=93), columns=['{} prediction'.format(model_name)])
-    df_nv_day_eval = pd.DataFrame(nv_day_eval, index=pd.date_range(end=end_day, periods=93), columns=['Naive error'])
-    df_md_day_eval = pd.DataFrame(md_day_eval, index=pd.date_range(end=end_day, periods=93), columns=['{} error'.format(model_name)])
+    df_day_true = pd.DataFrame(day_true, index=pd.date_range(end=ed_day, periods=93), columns=['True value'])
+    df_nv_day_pred = pd.DataFrame(nv_day_pred, index=pd.date_range(end=ed_day, periods=93), columns=['Naive predicton'])
+    df_md_day_pred = pd.DataFrame(md_day_pred, index=pd.date_range(end=ed_day, periods=93), columns=['{} prediction'.format(mdl_name)])
+    df_nv_day_eval = pd.DataFrame(nv_day_eval, index=pd.date_range(end=ed_day, periods=93), columns=['Naive error'])
+    df_md_day_eval = pd.DataFrame(md_day_eval, index=pd.date_range(end=ed_day, periods=93), columns=['{} error'.format(mdl_name)])
     df_day_eval = pd.concat([df_day_true, df_nv_day_pred, df_md_day_pred, df_md_day_eval, df_nv_day_eval], axis=1)
-    df_day_eval.to_csv(log_dir + 'df_{}{}_eval_day.csv'.format(model_name, model_version), index=None)
-    df_day_eval.loc[:, ['Naive predicton', '{} prediction'.format(model_name), 'True value']].plot()
-    plt.savefig(log_dir + 'fig_{}{}_pred_day_vs_nv.png'.format(model_name, model_version), transparent=True, bbox_inches='tight')
-    df_day_eval.loc[:, ['Naive error', '{} error'.format(model_name)]].plot()
-    plt.savefig(log_dir + 'fig_{}{}_eval_day_vs_nv.png'.format(model_name, model_version), transparent=True, bbox_inches='tight')
+    df_day_eval.to_csv(log_dir + 'df_{}{}_eval_day.csv'.format(mdl_name, ver), index=None)
+    df_day_eval.loc[:, ['Naive predicton', '{} prediction'.format(mdl_name), 'True value']].plot()
+    plt.savefig(log_dir + 'fig_{}{}_pred_day_vs_nv.png'.format(mdl_name, ver), transparent=True, bbox_inches='tight')
+    df_day_eval.loc[:, ['Naive error', '{} error'.format(mdl_name)]].plot()
+    plt.savefig(log_dir + 'fig_{}{}_eval_day_vs_nv.png'.format(mdl_name, ver), transparent=True, bbox_inches='tight')
 
     df_bin_true = pd.DataFrame(bin_true, index=latlon, columns=['True value'])
     df_nv_bin_pred = pd.DataFrame(nv_bin_pred, index=latlon, columns=['Naive predicton'])
-    df_md_bin_pred = pd.DataFrame(md_bin_pred, index=latlon, columns=['{} prediction'.format(model_name)])
+    df_md_bin_pred = pd.DataFrame(md_bin_pred, index=latlon, columns=['{} prediction'.format(mdl_name)])
     df_nv_bin_eval = pd.DataFrame(nv_bin_eval, index=latlon, columns=['Naive error'])
-    df_md_bin_eval = pd.DataFrame(md_bin_eval, index=latlon, columns=['{} error'.format(model_name)])
+    df_md_bin_eval = pd.DataFrame(md_bin_eval, index=latlon, columns=['{} error'.format(mdl_name)])
     df_bin_eval = pd.concat([df_bin_true, df_nv_bin_pred, df_md_bin_pred, df_md_bin_eval, df_nv_bin_eval], axis=1)
-    df_bin_eval.to_csv(log_dir + 'df_{}{}_eval_bin.csv'.format(model_name, model_version), index=None)
-    df_bin_eval.loc[:, ['Naive predicton', '{} prediction'.format(model_name), 'True value']].plot()
-    plt.savefig(log_dir + 'fig_{}{}_pred_bin_vs_nv.png'.format(model_name, model_version), transparent=True, bbox_inches='tight')
-    df_bin_eval.loc[:, ['Naive error', '{} error'.format(model_name)]].plot()
-    plt.savefig(log_dir + 'fig_{}{}_eval_bin_vs_nv.png'.format(model_name, model_version), transparent=True, bbox_inches='tight')
+    df_bin_eval.to_csv(log_dir + 'df_{}{}_eval_bin.csv'.format(mdl_name, ver), index=None)
+    df_bin_eval.loc[:, ['Naive predicton', '{} prediction'.format(mdl_name), 'True value']].plot()
+    plt.savefig(log_dir + 'fig_{}{}_pred_bin_vs_nv.png'.format(mdl_name, ver), transparent=True, bbox_inches='tight')
+    df_bin_eval.loc[:, ['Naive error', '{} error'.format(mdl_name)]].plot()
+    plt.savefig(log_dir + 'fig_{}{}_eval_bin_vs_nv.png'.format(mdl_name, ver), transparent=True, bbox_inches='tight')
 
     df_bin_eval = df_bin_eval.reset_index()
     df_bin_eval['lat'] = df_bin_eval['index'].astype(str).str[:2].astype(int)
@@ -353,19 +353,19 @@ def main():
         lo = row['lon'].astype(int)
         bin_trues[la+89, lo+179] = row['True value']
         nv_bin_preds[la+89, lo+179] = row['Naive predicton']
-        md_bin_preds[la+89, lo+179] = row['{} prediction'.format(model_name)]
+        md_bin_preds[la+89, lo+179] = row['{} prediction'.format(mdl_name)]
         nv_bin_evals[la+89, lo+179] = row['Naive error']
-        md_bin_evals[la+89, lo+179] = row['{} error'.format(model_name)]
+        md_bin_evals[la+89, lo+179] = row['{} error'.format(mdl_name)]
 
     plot_on_map(bin_trues, 0.3, 'true')
     plot_on_map(nv_bin_preds, 0.3, 'Naive_pred')
-    plot_on_map(md_bin_preds, 0.3, model_name+model_version+'_pred')
+    plot_on_map(md_bin_preds, 0.3, mdl_name+ver+'_pred')
     plot_on_map(nv_bin_evals, 2.0, 'Naive_eval')
-    plot_on_map(md_bin_evals, 2.0, model_name+model_version+'_eval')
+    plot_on_map(md_bin_evals, 2.0, mdl_name+ver+'_eval')
 
     if not record:
         return
-    now = datetime.datetime.now().strftime(date_format)
+    now = datetime.datetime.now().strftime(fmt)
     record_file = log_dir + 'record.csv'
     str_num_filters = list_to_str(num_filters)
     str_dropouts = list_to_str(dropouts)
@@ -373,14 +373,14 @@ def main():
     if os.path.exists(record_file):
         with open(log_dir + 'record.csv', 'a') as f:
             f.write('{},{},{}{},{},{},{},{},{},{},{},{}\n'
-                    .format(now, md_eval, model_name, model_version, str_num_filters, optimizer_name, lr, decay,
+                    .format(now, md_eval, mdl_name, ver, str_num_filters, opt_name, lr, decay,
                             str_dropouts, str_recurrent_dropouts, epochs,random_seed))
     else:
         with open(log_dir + 'record.csv', 'a') as f:
             f.write('date,eval,model,filt,optm,lr,decay,drpout,r_drpout,epch,seed\n')
             f.write('{},{},Naivemodel,None,None,None,None,None,None,None,None\n'.format(now, nv_eval))
             f.write('{},{},{}{},{},{},{},{},{},{},{},{}\n'
-                    .format(now, md_eval, model_name, model_version, str_num_filters, optimizer_name, lr, decay,
+                    .format(now, md_eval, mdl_name, ver, str_num_filters, opt_name, lr, decay,
                             str_dropouts, str_recurrent_dropouts, epochs,random_seed))
 
 if __name__ == '__main__':
